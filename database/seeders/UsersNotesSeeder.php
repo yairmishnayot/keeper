cac547f2a7a3bd3c469da1af11e895e07b0c0671
Yair Mishnayot <yair543210@gmail.com>
2022-03-04 15:42:04 +0200

    feat/created users notes connection and files

---
+++ database/seeders/UsersNotesSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersNotesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
