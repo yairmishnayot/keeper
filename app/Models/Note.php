7f51ef90304839222900448a9ec605a374d17394
Yair Mishnayot <yair543210@gmail.com>
2022-03-07 22:46:32 +0200

    feat/added ide-helper

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Note
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $content
+ * @property int $background
+ * @property int $is_background_image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\NoteFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Note newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Note query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereBackground($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereIsBackgroundImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Note whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Note extends Model
 {
     use HasFactory;
--- app/Models/UsersNotes.php
+++ app/Models/UsersNotes.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * App\Models\UsersNotes
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $note_id
+ * @property int $role
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes whereNoteId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UsersNotes whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UsersNotes extends Pivot
 {
     //
