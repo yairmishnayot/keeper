7da679316f98c9b2526b1fe1e7c8614985eb4849
Yair Mishnayot <yair543210@gmail.com>
2022-03-24 21:39:17 +0200

    feat/created admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+           'name' => 'Yair',
+            'email' => 'yair543210@gmail.com',
+            'password' => bcrypt('secret123')
+        ]);
+
+
+    }
+}
